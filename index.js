eec31257db6f8164623e9aedc1895a89c4c37313
Sparkenstein <zetabytes.pp@gmail.com>
2018-11-21 21:42:57 +0530

    module is stable now

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const fs = require('fs');
-const { log } = console;
-const content = fs.readFileSync('./sample.ini', 'utf-8');
 
 // regexes
 const sectionRE = /\[(.*?)\]/;
 const commentsRE = /^\s*;/;
 
-const filtered = content.split('\n').filter(e => e && !e.match(commentsRE));
+class iniParser {
+    constructor(fileName) {
+        const content = fs.readFileSync(fileName, 'utf-8');
+        this.filtered = content.split('\n').filter(e => e && !e.match(commentsRE));
 
-/** TODO: Change imperative parts to declarative */
+        this.relations = {};
+        let currentSection = '';
+        for (let i of this.filtered) {
+            if (i.match(sectionRE)) {
+                currentSection = i.match(sectionRE)[1];
+                this.relations[currentSection] = {};
+            } else {
+                const pairs = i.trim().split("=");
+                this.relations[currentSection][pairs[0]] = pairs[1];
+            }
+        }
+    }
 
-let relations = {};
-let currentSection = '';
-for (let i of filtered) {
-    if (i.match(sectionRE)) {
-        currentSection = i.match(sectionRE)[1];
-        relations[currentSection] = {};
-    } else {
-        const pairs = i.trim().split("=");
-        relations[currentSection][pairs[0]] = pairs[1];
+    getsections() {
+        return this.filtered.reduce((a, e) => {
+            const m = e.match(/\[(.*?)\]/);
+            if (m)
+                a.push(m[1]);
+            return a;
+        }, []);
+    }
+
+    getKeys(sectionName) {
+        return Object.keys(this.relations[sectionName]);
+    }
+    
+    getValue(sectionName, key){
+        return this.relations[sectionName][key];
     }
 }
+
+module.exports = iniParser;
